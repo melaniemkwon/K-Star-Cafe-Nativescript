fb399d5fe5358906ac28af1f8042abb352538b19
melaniemkwon <melaniemkwon@gmail.com>
2018-01-04 22:07:05 -0800

    Navigation for Dish Detail Component
    -Add DishdetailComponent.
    -Use NativeScript navigation support to navigate from menu to dishdetail.

--- app/menu/menu.component.ts
+++ app/menu/menu.component.ts
@@ -5,8 +5,8 @@ import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-menu',
   moduleId: module.id,
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  templateUrl: './menu.component.html'
+  // styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
   dishes: Dish[];
