f3204d13c2f5546d049c520bf80927758793decd
melaniemkwon <melaniemkwon@gmail.com>
2018-01-15 14:33:55 -0800

    Platform and Network Status
    -Use the platform class to obtain device related information including its screen size.
    -Use the connectivity class to obtain the connectivity type, and also watch for changes in network connectivity.

--- app/app.module.ts
+++ app/app.module.ts
@@ -27,6 +27,7 @@ import { LeaderService } from "./services/leader.service";
 import { FavoriteService } from "./services/favorite.service";
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 import { CouchbaseService } from './services/couchbase.service';
+import { PlatformService } from './services/platform.service';
 
 import { baseURL } from "./shared/baseurl";
 
@@ -71,6 +72,7 @@ import { baseURL } from "./shared/baseurl";
         FavoriteService,
         ProcessHTTPMsgService,
         CouchbaseService,
+        PlatformService,
         { provide: 'BaseURL', useValue: baseURL }
     ],
     schemas: [
