9e871cff1b63e3da5982076d1a4fcb98679769fc
melaniemkwon <melaniemkwon@gmail.com>
2018-01-14 21:53:41 -0800

    Animations and Storage for Reservations
    -When the user fills the reservation form and submits the form, then the reservation form will be hidden with animation. The animation should be triggered when the submit button is clicked.
    -The reservation form is hidden and the submitted information is processed and then the submitted form information will be animated onto the screen.
    -When the form is submitted, the form information should be saved in the Couchbase lite database.

--- app/reservation/reservation.component.ts
+++ app/reservation/reservation.component.ts
@@ -5,26 +5,47 @@ import { Switch } from 'ui/switch';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/modal-dialog';
 import { ReservationModalComponent } from '../reservationmodal/reservationmodal.component';
+import { Animation, AnimationDefinition } from "ui/animation";
+import { View } from "ui/core/view";
+import { Page } from "ui/page";
+import * as enums from "ui/enums";
+import { CouchbaseService } from '../services/couchbase.service';
 
 @Component({
-    selector: 'reservation-menu',
+    selector: 'app-reservation',
     moduleId: module.id,
     templateUrl: './reservation.component.html'
   })
 export class ReservationComponent extends DrawerPage implements OnInit {
-    reservation: FormGroup;
+    reservationForm: FormGroup;
+    reservationFormView: View;
+    reservationSubmissionView: View;
+    showSubmission: boolean = false;
+    reservations: Array<number>;
+    docId: string = "reservations";
 
     constructor(private changeDetectorRef: ChangeDetectorRef,
         private modalService: ModalDialogService,
         private vcRef: ViewContainerRef,
-        private formBuilder: FormBuilder) {
+        private formBuilder: FormBuilder,
+        private page: Page,
+        private couchbaseService: CouchbaseService) {
             super(changeDetectorRef);
 
-            this.reservation = this.formBuilder.group( {
+            this.reservationForm = this.formBuilder.group( {
                 guests: 3,
                 smoking: false,
                 dateTime: ['', Validators.required]
             });
+
+            this.reservations = [];
+            let doc = this.couchbaseService.getDocument(this.docId);
+            if( doc == null) {
+                this.couchbaseService.createDocument({"reservations": []}, this.docId);
+            }
+            else {
+                this.reservations = doc.favorites;
+            }
     }
     
     ngOnInit() {
@@ -34,20 +55,20 @@ export class ReservationComponent extends DrawerPage implements OnInit {
     onSmokingChecked(args) {
         let smokingSwitch = <Switch>args.object;
         if (smokingSwitch.checked) {
-            this.reservation.patchValue({ smoking: true });
+            this.reservationForm.patchValue({ smoking: true });
         } else {
-            this.reservation.patchValue({ smoking: false });
+            this.reservationForm.patchValue({ smoking: false });
         }
     }
 
     onGuestChange(args) {
         let textField = <TextField>args.object;
-        this.reservation.patchValue({ guests: textField.text });
+        this.reservationForm.patchValue({ guests: textField.text });
     }
 
     onDateTimeChange(args) {
         let textField = <TextField>args.object;
-        this.reservation.patchValue({ dateTime: textField.text });
+        this.reservationForm.patchValue({ dateTime: textField.text });
     }
     
     createModalView(args) {
@@ -60,15 +81,51 @@ export class ReservationComponent extends DrawerPage implements OnInit {
         this.modalService.showModal(ReservationModalComponent, options)
             .then((result: any) => {
                 if (args === "guest") {
-                    this.reservation.patchValue({guests: result});
+                    this.reservationForm.patchValue({guests: result});
                 } else if (args === "date-time") {
-                    this.reservation.patchValue({dateTime: result});
+                    this.reservationForm.patchValue({dateTime: result});
                 }
             });
     }
 
     onSubmit() {
-        console.log(JSON.stringify(this.reservation.value));
+        this.addReservation();
+
+        this.reservationFormView = this.page.getViewById<View>("reservationForm");
+        this.reservationFormView.animate({
+            scale: { x: 0, y: 0 },
+            opacity: 0,
+            duration: 500,
+            curve: enums.AnimationCurve.easeIn
+        }).then(() => {
+            console.log("reservationFormView animation played");
+            this.reservationSubmissionView = this.page.getViewById<View>("reservationSubmission");
+            this.reservationSubmissionView.scaleX = 0.1;
+            this.reservationSubmissionView.scaleY = 0.1;
+            this.reservationSubmissionView.opacity = 0;
+
+            this.showSubmission = true;
+
+            this.reservationSubmissionView.animate({
+                scale: { x: 1, y: 1 },
+                opacity: 1,
+                duration: 500,
+                curve: enums.AnimationCurve.easeIn
+            }).then(() => {
+                console.log("reservationSubmissionView animations played");
+            }).catch((e) => {
+                console.log(e.message);
+            });
+        }).catch((e) => {
+            console.log(e.message);
+        });
+    }
+
+    addReservation(): boolean {
+        console.log(JSON.stringify(this.reservationForm.value));
+        this.reservations.push(this.reservationForm.value);
+        this.couchbaseService.updateDocument(this.docId, {"reservations": this.reservations});
+        return true;
     }
 }
 
